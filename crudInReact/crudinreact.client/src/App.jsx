c697821b496666ff12d630230ecd5703256e24ea
OM SHARMA <developeromsharma@gmail.com>
2025-05-11 18:47:23 +0530

    update placeholder text and vite config

--- crudInReact/crudinreact.client/src/App.jsx
+++ crudInReact/crudinreact.client/src/App.jsx
@@ -5,7 +5,7 @@ import './App.css';
 function App() {
     return (
         <div className="App">
-            <h1>Course Management</h1>
+            <h1>CRUD Operation in React</h1>
             <CourseCrud />
         </div>
     );
